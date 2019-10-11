0ee95f77a4383eab01c9f738ce9065ec4c6e2469
Hiroki, Hattori <seagull.kamome@gmail.com>
2019-10-11 23:27:54 +0900

    :recycle: :sparcles: add atomic operation interface of carrotlib.
      - add new file carrotlib/atomic.h
      - add new configuration variables.
      - delete obsoleted macros.

---
+++ carrotlib/include/carrotlib/atomic.h
@@ -0,0 +1,83 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Hattori, Hiroki
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/** @file
+ *  @brief Interface of atomic operation
+ */
+#pragma once
+
+
+#define CARROT_ATOMIC_IMPL_NONE         (0 /* Dummy impl */)
+#define CARROT_ATOMIC_IMPL_STD          (1 /* Use stdatomic */)
+#define CARROT_ATOMIC_IMPL_ARCH         (2 /* Use carrotlib/arch */)
+#define CARROT_ATOMIC_IMPL_MCU          (3 /* Use carrotlib/mcu */)
+#define CARROT_ATOMIC_IMPL_BOARD        (4 /* Use carrotlib/board */)
+#define CARROT_ATOMIC_IMPL_CONFIG       (5 /* Use carrotlib/port */)
+
+
+#include <carrotlib/config.h>
+
+#if !defined(CARROT_CONFIG_ATOMIC_IMPL)
+#  error "Need configure CARROT_CONFIG_ATOMIC_IMPL"
+#elif (CARROT_CONFIG_ATOMIC_IMPL == CARROT_ATOMIC_IMPL_NONE)
+
+#  if defined(CARROT_CONFIG_SMP) && CARROT_CONFIG_SMP
+#    warning "configured CARROT_ATOMIC_IMPL_NONE but CARROT_CONFIG_SMP,"
+#  else
+#    define __atomic
+#    define CARROT_FENCE()        do { } while (0)
+#    define CARROT_READ_ONCE(ptr) (*(ptr))
+#    define CARROT_WRITE_ONCE(ptr, val) do { *(ptr) = (val); } while (0)
+#    define CARROT_COMPARE_EXCHANGE(object, expected, desired) ( \
+    (*(object) == *(expected)) \
+      ? ((*(object) = desired), true) \
+      : ((*(expected) = *(object)), false) )
+#  endif
+
+
+#elif (CARROT_CONFIG_ATOMIC_IMPL == CARROT_ATOMIC_IMPL_GENERIC)
+
+#  include <stdatomic.h>
+
+#  define __atomic
+#  define CARROT_FENCE()                do { atomic_thread_fence(memory_order_seq_cst); } while (0)
+#  define CARROT_READ_ONCE(ptr)         do { atomic_load((ptr), (val)); } while (0)
+#  define CARROT_WRITE_ONCE(ptr, val)   do { atomic_store((ptr), (val)); } while (0)
+#  define CARROT_COMPARE_EXCHANGE(object, expected, desired) \
+  atomic_compare_exchange_strong((object), (expected(, (desired(, memory_order_seq_cst))
+
+#elif (CARROT_CONFIG_ATOMIC_IMPL == CARROT_ATOMIC_IMPL_ARCH)
+#  include <carrotlib/arch/atomic.h>
+#elif (CARROT_CONFIG_ATOMIC_IMPL == CARROT_ATOMIC_IMPL_MCU)
+#  include <carrotlib/mcu/atomic.h>
+#elif (CARROT_CONFIG_ATOMIC_IMPL == CARROT_ATOMIC_IMPL_BOARD)
+#  include <carrotlib/board/atomic.h>
+#elif (CARROT_CONFIG_ATOMIC_IMPL == CARROT_ATOMIC_IMPL_CONFIG)
+#  include <carrotlib/config/atomic.h>
+#else
+#  error "Bad CARROT_CONFIG_ATOMIC_IMPL"
+#endif
+
+
+// vim: ts=8 sw=2 cindent expandtab :
