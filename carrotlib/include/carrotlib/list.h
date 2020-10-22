f07e24cf3e3aac34eec0f50612fb6209bed35c48
HATTORI,Hiroki <seagull.kamome@gmail.com>
2020-10-22 20:36:15 +0900

    indent

--- carrotlib/include/carrotlib/list.h
+++ carrotlib/include/carrotlib/list.h
@@ -37,23 +37,23 @@
 extern "C" {
 #endif
 
-typedef struct carrot_list_head {
+struct carrot_list {
   struct carrot_list_head *next;
   struct carrot_list_head *prev;
-} carrot_list_head_t;
+};
 
 
 #define CARROT_LIST_INITIALIZER(name)  { .next = &(name), .prev = &(name) }
 
-static __forceinline void carrot_list_init(carrot_list_head_t *it) {
+static __forceinline void carrot_list_init(struct carrot_list *it) {
   it->next = it;
   it->prev = it;
 }
 
 static __forceinline void carrot_list_insert(
-    carrot_list_head_t *prev,
-    carrot_list_head_t *next,
-    carrot_list_head_t *it) {
+    struct carrot_list *prev,
+    struct carrot_list *next,
+    struct carrot_list *it) {
   it->prev = prev;
   it->next = next;
   next->prev = it;
@@ -62,52 +62,36 @@ static __forceinline void carrot_list_insert(
 
 
 static __forceinline void carrot_list_connect(
-    carrot_list_head_t *prev,
-    carrot_list_head_t *next) {
+    struct carrot_list *prev,
+    struct carrot_list *next) {
   next->prev = prev;
   prev->next = next;
 }
 
 
-static __forceinline void carrot_list_add(
-    carrot_list_head_t *whence, carrot_list_head_t *it) {
-  carrot_list_insert(whence, whence->next, it); }
-static __forceinline void carrot_list_add_tail(
-    carrot_list_head_t *whence, carrot_list_head_t *it) {
-  carrot_list_insert(whence->prev, whence, it); }
+static __forceinline void carrot_list_add(struct carrot_list *whence, struct carrot_list *it) { carrot_list_insert(whence, whence->next, it); }
+static __forceinline void carrot_list_add_tail(struct carrot_list *whence, struct carrot_list *it) { carrot_list_insert(whence->prev, whence, it); }
 
-static __forceinline void carrot_list_remove(carrot_list_head_t *it) {
-  carrot_list_connect(it->prev, it->next); }
-static __forceinline void carrot_list_remove_and_init(carrot_list_head_t *it) {
-  carrot_list_remove(it); carrot_list_init(it); }
+static __forceinline void carrot_list_remove(struct carrot_list *it) { carrot_list_connect(it->prev, it->next); }
+static __forceinline void carrot_list_remove_and_init(struct carrot_list *it) { carrot_list_remove(it); carrot_list_init(it); }
 
-static __forceinline void carrot_list_move(
-    carrot_list_head_t* whence, carrot_list_head_t *it) {
-  carrot_list_remove(it); carrot_list_add(whence, it); }
-static __forceinline void carrot_list_move_to_tail(
-    carrot_list_head_t* head, carrot_list_head_t *it) {
-  carrot_list_remove(it); carrot_list_add_tail(head, it); }
+static __forceinline void carrot_list_move(struct carrot_list* whence, struct carrot_list *it) { carrot_list_remove(it); carrot_list_add(whence, it); }
+static __forceinline void carrot_list_move_to_tail(struct carrot_list* head, struct carrot_list *it) { carrot_list_remove(it); carrot_list_add_tail(head, it); }
 
 
 __nodiscard
-static __forceinline bool carrot_list_empty(carrot_list_head_t const *it) {
-  return it->next == it; }
+static __forceinline bool carrot_list_empty(struct carrot_list const *it) { return it->next == it; }
 
 __nodiscard
-static __forceinline bool carrot_list_singleton(carrot_list_head_t const *it) {
-  return !carrot_list_empty(it) && it->next == it->prev; }
+static __forceinline bool carrot_list_singleton(struct carrot_list const *it) { return !carrot_list_empty(it) && it->next == it->prev; }
 
 __nodiscard
-static __forceinline bool carrot_list_is_first(
-    carrot_list_head_t const *head, carrot_list_head_t const *it) {
-  return head->next == it; }
+static __forceinline bool carrot_list_is_first(struct carrot_list const *head, struct carrot_list const *it) { return head->next == it; }
 
 __nodiscard
-static __forceinline bool carrot_list_is_tail(
-    carrot_list_head_t const *head, carrot_list_head_t const *it) {
-  return head->prev == it; }
+static __forceinline bool carrot_list_is_tail(struct carrot_list const *head, struct carrot_list const *it) { return head->prev == it; }
 
-static __forceinline void carrot_list_rotate(carrot_list_head_t *it) {
+static __forceinline void carrot_list_rotate(struct carrot_list *it) {
   if (!carrot_list_empty(it)) carrot_list_move_to_tail(it->next, it); }
 
 #define CARROT_LIST_FOR_EACH(typ, iter, head) \
